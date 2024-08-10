d1e9d852b04e9eb0ea4dc28f945cdf6256b81359
Sencyy <sencyy@protonmail.com>
2024-08-10 12:45:20 -0300

    improving installation

--- installdots.sh
+++ installdots.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-find -mindepth 1 -maxdepth 1 -type d | while read dir; do
-  fmtdir="$(sed -e s/\\.\\///g <<<$dir)"
-  if [ $(echo $fmtdir) != ".git" ]; then
-    stow -t "$HOME" "$fmtdir"
-    echo "installed $fmtdir configuration"
-  else
-    echo "ommiting $fmtdir directory"
-  fi
+find -mindepth 1 -maxdepth 1 -type d -not -path '*/\.*' | while read dir; do
+  fmtdir="$(sed s/\\.\\///g <<<$dir)"
+  stow -t "$HOME" "$fmtdir"
+  echo "installed $fmtdir configuration"
 done
